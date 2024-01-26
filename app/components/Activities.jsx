381bf373a3ea9f5f07f9d158682cc710aaac0748
chakir elarabi <116119429+codewithchakir@users.noreply.github.com>
2024-01-26 02:49:38 +0100

    removed overflow hidden

--- app/components/Activities.jsx
+++ app/components/Activities.jsx
@@ -6,7 +6,7 @@ import Variables from '@/public/var/vriables';
 
 function  Activities () {
 return (
-  <section className='p-1 sm:p-4 flex flex-col items-center overflow-x-hidden max-w-screen-xl mx-auto' id='activities'>
+  <section className='p-1 sm:p-4 flex flex-col items-center max-w-screen-xl mx-auto' id='activities'>
       <h1 className='text-4xl font-medium mb-8'>Our Activities</h1>
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-16 lg:gap-20 xl:gap-28'>
       {Variables.activitiesData.map((cardInfo, index) => (
--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -4,10 +4,16 @@ const Contact = () => (
       <section className="contact mx-auto max-w-screen-xl md:p-4 " id="contact">
         <h2>Contact Us</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+
+          
           <div className="border-gray-200 border-4 rounded-2xl overflow-hidden">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1235.3889538467563!2d-7.94185236398196!3d31.695827734374273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdaff3b891004267%3A0x57570898d47ea5de!2sQuad%20Adventure%20Marrakesh%20-%20quad%20marrakech!5e0!3m2!1sen!2sma!4v1705318728069!5m2!1sen!2sma"
           width="100%" height="100%" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
+
+
+
           <div className="border-gray-200 border-4 p-2 md:p-4 rounded-2xl">
             <form className=" p-3 md:p-6 ">
               <div className="mb-4">
